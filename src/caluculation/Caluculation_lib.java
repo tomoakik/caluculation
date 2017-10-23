13e280cd623964512267e03cf053d5a1aaed767c
50616013 <50616013@50616013-pc>
2017-10-23 17:10:14 +0900

    caluculation

---
+++ src/caluculation/Caluculation_lib.java
@@ -0,0 +1,32 @@
+package caluculation;
+
+public class Caluculation_lib {
+    private int m,n;
+
+	public Caluculation_lib(int m, int n) {
+		this.m = m;
+		this.n = n;
+	}
+    public int getPlus() {
+    	return this.m + this.n;
+    }
+    public int getMinus() {
+        return this.m - this.n;
+    }
+    public int getTime() {
+       	return this.m * this.n;
+    }
+   	public int getDivide() {
+        return this.m / this.n;
+   	}
+    public int getMod() {
+        	return this.m % this.n;
+    }
+    
+	
+
+
+
+
+    }
+
